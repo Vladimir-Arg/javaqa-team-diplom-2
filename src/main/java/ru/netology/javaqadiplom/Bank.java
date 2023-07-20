8d2123750a8edfd45d642842fb8c6b93fe396245
Asaulka Ksenia <asa.ru.80@mail.ru>
2023-07-20 11:29:43 +0300

    Bug fixed issues #16

--- src/main/java/ru/netology/javaqadiplom/Bank.java
+++ src/main/java/ru/netology/javaqadiplom/Bank.java
@@ -8,8 +8,9 @@ public class Bank {
      * уменьшиться на эту сумму, а баланс счёта to увеличиться.
      * Если операция прошла неуспешно, балансы обоих счетов никак
      * измениться не должны.
-     * @param from - счёт с которого переводим
-     * @param to - счёт на который переводим
+     *
+     * @param from   - счёт с которого переводим
+     * @param to     - счёт на который переводим
      * @param amount - сумма перевода
      * @return - true если операция прошла успешно, false иначе
      */
@@ -18,7 +19,14 @@ public class Bank {
             return false;
         }
         if (from.pay(amount)) {
-            to.add(amount);
+            from.add(amount);
+            if (to.add(amount)) {
+                to.pay(amount);
+
+                from.pay(amount);
+                to.add(amount);
+                return true;
+            }
         }
         return true;
     }
